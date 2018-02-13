05fc25b2c26fbd0a7e94d488ba94d0f5e769e847
vagrant <vagrant@homestead>
2018-02-13 06:59:12 +0000

    Generated topics

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
 	{
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
+		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
 		\App\Models\Project::observe(\App\Observers\ProjectObserver::class);
 
         // 时间戳created_at 和updated_at 作为模型属性被调用是，都会自动转换为Carbon对象
