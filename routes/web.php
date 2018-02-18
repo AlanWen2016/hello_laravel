b1c6686bf022fb958b873ce3fcee7fdfb30eba28
vagrant <vagrant@homestead>
2018-02-18 14:47:02 +0000

    User can upload topic Image

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,6 @@ Route::resource('projects', 'ProjectsController', ['only' => ['index', 'show', '
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+// 文件上传
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
